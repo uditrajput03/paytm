d991748aea732846cbfa250b8f8b897752ba779a
Udit Rajput <87136581+uditrajput03@users.noreply.github.com>
2024-02-16 16:32:22 +0000

    Updated models

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,10 +1,19 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    email:String,
-    password:String
+    firstName: {
+        type: String,
+        required: true},
+    lastName: {
+        type: String,
+        required: true},
+    email:{
+        type: String,
+        required: true,
+        unique: true},
+    password:{
+        type: String,
+        required: true}
 })
 const User = new mongoose.model('User' , userSchema);
 export default User;
