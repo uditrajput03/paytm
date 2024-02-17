497b1ab611a2ee8288198f57826b172d4caed15c
Udit Rajput <87136581+uditrajput03@users.noreply.github.com>
2024-02-17 19:23:43 +0000

    Added user router

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const { default: connectDb } = require('./db');
 const app = express();
+const routes = require('./routes/index')
+const userRouter = require('./routes/user')
+app.use('/api/v1' , routes);
+app.use('/api/v1/user' , userRouter);
 
-console.log(typeof connectDb);
+//console.log(typeof connectDb);
