f8251b0e8a138d7263458e5328c1d6a7177ef754
Udit Rajput <87136581+uditrajput03@users.noreply.github.com>
2024-02-25 15:10:36 +0000

    Added port in env

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 const { default: connectDb } = require('./db');
 const app = express();
 const routes = require('./routes/index');
@@ -10,6 +11,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/v1', routes);
 
-app.listen(3000, () => {
+app.listen((process.env.PORT ||3000), () => {
     console.log("Listen on port 3000");
 });
