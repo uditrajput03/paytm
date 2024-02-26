5aba9c3d7e76ebe5ffcdd713fe3c340a63cf97e5
Udit Rajput <87136581+uditrajput03@users.noreply.github.com>
2024-02-26 18:24:15 +0000

    Added transfer route alternative

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -54,3 +54,49 @@ accountRouter.post('/transfer' , authMiddleware , async (req , res) => {
 module.exports = {
     accountRouter
 }
+
+//Bade code for transaction Instead use transaction Functions in DB.
+
+// router.post("/transfer", authMiddleware, async (req, res) => {
+//     const { amount, to } = req.body;
+
+//     const account = await Account.findOne({
+//         userId: req.userId
+//     });
+
+//     if (account.balance < amount) {
+//         return res.status(400).json({
+//             message: "Insufficient balance"
+//         })
+//     }
+
+//     const toAccount = await Account.findOne({
+//         userId: to
+//     });
+
+//     if (!toAccount) {
+//         return res.status(400).json({
+//             message: "Invalid account"
+//         })
+//     }
+
+//     await Account.updateOne({
+//         userId: req.userId
+//     }, {
+//         $inc: {
+//             balance: -amount
+//         }
+//     })
+
+//     await Account.updateOne({
+//         userId: to
+//     }, {
+//         $inc: {
+//             balance: amount
+//         }
+//     })
+
+//     res.json({
+//         message: "Transfer successful"
+//     })
+// });
