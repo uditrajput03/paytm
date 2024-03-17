ddb07953305977b669eeee3babd9b8ea529dab74
Udit Rajput <87136581+uditrajput03@users.noreply.github.com>
2024-03-18 04:06:04 +0530

    Add error in login and signup page

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -8,41 +8,53 @@ import axios from "axios"
 import { Link, useNavigate } from "react-router-dom"
 
 const ErrorMessage = ({ id, message }) => {
-    return (
-      <p
-        id={id}
-        className="text-destructive text-[14px] font-semibold absolute -bottom-6 right-1/2 translate-x-1/2"
-        aria-live="polite"
-        aria-atomic="true"
-      >
-        <i>{message}</i>
-      </p>
-    );
-  };
+  return (
+    <p
+      id={id}
+      className="text-[14px] font-semibold text-center text-red-700 pb-2"
+    >
+      <i>{message}</i>
+    </p>
+  );
+};
 
 function Signin() {
-    const [email , setEmail] = useState("")
-    const [password , setPassword] = useState("")
-    const navigate = useNavigate()
-    return(
-        <div className="flex flex-col w-full h-full">
-            <Heading label="Sign In"/>
-            <SubHeading label="Enter your credentials to access your account"/>
-            <InputBox onChange={e => setEmail(e.target.value)} label="Email" type="email"/>
-            <InputBox onChange={e => setPassword(e.target.value)} label="Password" type="password"/>
-            {error && <ErrorMessage id="login-error" message={error} />}
-            <Button onClick={async () => {
-                let res = await axios.post(import.meta.env.VITE_BACKEND_URL + "/api/v1/user/signin" , {
-                    email,
-                    password
-                })
-                if(res.data.token){
-                    localStorage.setItem('token' , res.data.token);
-                    navigate('/dashboard')
-                }
-            }} label="SignIn"/>
-            <AuthRedirect label="Don't have an account? " page="Sign Up" link="/Signup"/>
-        </div>
-    )
+  const [error, setError] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const navigate = useNavigate()
+  return (
+    <div className="flex flex-col w-full h-full">
+      <Heading label="Sign In" />
+      <SubHeading label="Enter your credentials to access your account" />
+      <InputBox onChange={e => setEmail(e.target.value)} label="Email" type="email" />
+      <InputBox onChange={e => setPassword(e.target.value)} label="Password" type="password" />
+      {error && <ErrorMessage id="login-error" message={error} />}
+      <Button onClick={async () => {
+        try {
+          let res = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/v1/user/signin`, {
+            email,
+            password
+          });
+          if (res.data.token) {
+            localStorage.setItem('token', res.data.token);
+            navigate('/dashboard');
+          } else {
+            setError(res.data.message);
+          }
+        } catch (error) {
+          console.error("Error during sign in:", error);
+          if (error.response.data.message) {
+            setError(error.response.data.message);
+          }
+          else {
+            setError("An error occurred during sign in. Please try again later.");
+          }
+        }
+
+      }} label="SignIn" />
+      <AuthRedirect label="Don't have an account? " page="Sign Up" link="/Signup" />
+    </div>
+  )
 }
 export default Signin
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -7,33 +7,58 @@ import SubHeading from "../components/SubHeading"
 import axios from 'axios'
 import { useNavigate } from "react-router-dom"
 
+const ErrorMessage = ({ id, message }) => {
+    return (
+        <p
+            id={id}
+            className="text-[14px] font-semibold text-center text-red-700 pb-2"
+        >
+            <i>{message}</i>
+        </p>
+    );
+};
+
 function Signup() {
-    const [firstName , setFirstName] = useState("")
-    const [lastName , setLastName] = useState("")
-    const [email , setEmail] = useState("")
-    const [password , setPassword] = useState("")
+    const [error, setError] = useState("")
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
     const navigate = useNavigate()
-    return(
+    return (
         <div className="flex flex-col w-full h-full">
-            <Heading label="Sign Up"/>
-            <SubHeading label="Enter your information to create an account"/>
-            <InputBox onChange={e => setFirstName(e.target.value)} label="First Name" placeholder="John"/>
-            <InputBox onChange={e => setLastName(e.target.value)} label="Last Name" placeholder="Doe"/>
-            <InputBox onChange={e => setEmail(e.target.value)} label="Email" placeholder="johndoe@example.com" type="email"/>
-            <InputBox onChange={e => setPassword(e.target.value)} label="Password" type="password"/>
-            <Button onClick={ async () => {
-                let res = await axios.post(import.meta.env.VITE_BACKEND_URL + '/api/v1/user/signup' , {
-                    firstName,
-                    lastName,
-                    email,
-                    password
-                })
-                if(res.data.token){
-                    localStorage.setItem('token' , res.data.token);
-                    navigate('/dashboard')
+            <Heading label="Sign Up" />
+            <SubHeading label="Enter your information to create an account" />
+            <InputBox onChange={e => setFirstName(e.target.value)} label="First Name" placeholder="John" />
+            <InputBox onChange={e => setLastName(e.target.value)} label="Last Name" placeholder="Doe" />
+            <InputBox onChange={e => setEmail(e.target.value)} label="Email" placeholder="johndoe@example.com" type="email" />
+            <InputBox onChange={e => setPassword(e.target.value)} label="Password" type="password" />
+            {error && <ErrorMessage id="login-error" message={error} />}
+            <Button onClick={async () => {
+                try {
+
+
+                    let res = await axios.post(import.meta.env.VITE_BACKEND_URL + '/api/v1/user/signup', {
+                        firstName,
+                        lastName,
+                        email,
+                        password
+                    })
+                    if (res.data.token) {
+                        localStorage.setItem('token', res.data.token);
+                        navigate('/dashboard')
+                    }
+                } catch (error) {
+                    console.error("Error during sign in:", error);
+                    if (error.response.data.message) {
+                        setError(error.response.data.message);
+                    }
+                    else {
+                        setError("An error occurred during sign in. Please try again later.");
+                    }
                 }
-            }} label="Sign Up"/>
-            <AuthRedirect label="Already have an account? " page="Sign In" link="/Signin"/>
+            }} label="Sign Up" />
+            <AuthRedirect label="Already have an account? " page="Sign In" link="/Signin" />
         </div>
     )
 }
