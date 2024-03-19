4d2b809c9024b9e7db0fa2a85cead627c5218584
Udit Rajput <87136581+uditrajput03@users.noreply.github.com>
2024-03-19 20:09:37 +0530

    Fix crash in route

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,11 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/v1', routes);
 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong! Internal Server Error' });
+});
+
 app.listen((process.env.PORT || 3000), () => {
     console.log("Listen on port 3000");
 });
